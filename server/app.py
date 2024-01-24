1b00c43ff234d7f2c80aa64db7db610ecc5cad0b
Sheila Chebii <shillerchier055@gmail.com>
2024-01-24 06:44:12 +0300

    done

--- server/app.py
+++ server/app.py
@@ -19,15 +19,56 @@ def home():
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal=Animal.query.filter(Animal.id==id).first()
+    return f'''
+            <ul>ID:{animal.id}</ul>
+            <ul>Name:{animal.name}</ul>
+            <ul>Species:{animal.species}</ul>
+            <ul>Zookeeper:{animal.zookeeper.name}</ul>
+            <ul>Enclosure:{animal.enclosure.environment}</ul>
+
+'''
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper=Zookeeper.query.filter(Zookeeper.id==id).first()
+
+    if not zookeeper:
+        return '<h1>404 Zookeeper not found</h1>'
+    animals=[animal for animal in zookeeper.animals]
+
+    response_body=f'''
+        <ul>ID:{zookeeper.id}</ul>
+        <ul>Name:{zookeeper.name}</ul>
+        <ul>Birthday:{zookeeper.birthday}</ul>
+                '''
+    if not animals:
+        response_body += f"<h1>{zookeeper.name} has no animals currently</h1>"
+    else:
+        for animal in animals:
+            response_body += f"<ul>Animal:{animal.name}</ul>"
+
+    response=make_response(response_body,200)
+    return response
+    
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
+    enclosure=Enclosure.query.filter(Enclosure.id==id).first()
+    animals=[animal for animal in enclosure.animals]
+
+    response_body=f'''
+        <ul>ID:{enclosure.id}</ul>
+        <ul>Environment:{enclosure.environment}</ul>
+        <ul>Open To Visitors:{enclosure.open_to_visitors}</ul>
+                '''
+    if not animals:
+        response_body += f"<h1>{enclosure.environment} has no animals currently</h1>"
+    else:
+        for animal in animals:
+            response_body += f"<ul>Animal:{animal.name}</ul>"
+    response=make_response(response_body,200)
+    return response     
 
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -7,17 +7,37 @@ metadata = MetaData(naming_convention={
 
 db = SQLAlchemy(metadata=metadata)
 
+# The Zookeeper model should contain a String name, a String birthday, and a list of animals that they take care of using db.relationship().
+
 class Zookeeper(db.Model):
     __tablename__ = 'zookeepers'
 
     id = db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String)
+    birthday=db.Column(db.String)
+    animals = db.relationship('Animal', backref='zookeeper')
 
+
+# The Enclosure model should contain a String environment (grass, sand, or water), a Boolean open_to_visitors, and a list of animals using db.relationship().
 class Enclosure(db.Model):
     __tablename__ = 'enclosures'
 
     id = db.Column(db.Integer, primary_key=True)
+    environment=db.Column(db.String)
+    open_to_visitors=db.Column(db.Boolean)
 
+    animals = db.relationship('Animal', backref='enclosure')
+
+    def __repr__(self):
+        return f''
+# The Animal model should contain a String name, a String species, a zookeeper_id, and an enclosure_id. It should be related to zookeepers and enclosures using db.relationship().
+# Reminder: You can test any model by itself with pytest testing/models/{modelname}_test.py.
 class Animal(db.Model):
     __tablename__ = 'animals'
 
     id = db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String)
+    species=db.Column(db.String)
+    zookeeper_id=db.Column(db.Integer, db.ForeignKey('zookeepers.id'))
+    enclosure_id=db.Column(db.Integer, db.ForeignKey('enclosures.id'))
+
